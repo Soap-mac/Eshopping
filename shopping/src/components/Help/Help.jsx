e43f46d22818520c378c9d01c46f77cdeab9d54e
Arpit Mishra <arpitmishra5096@gmail.com>
2026-08-30 15:43:28 +0530

    Fix production server configuration

--- shopping/src/components/Help/Help.jsx
+++ shopping/src/components/Help/Help.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Top from '../top/top'
+import Top from '../Top/Top'
 
 function Help() {
   return (
     <div>
-        <Top />
+      <Top />
     </div>
   )
 }
